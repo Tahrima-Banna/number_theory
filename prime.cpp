ae9ba8455ced7176efe55e8af8f1e9a92730718a
Tahrima-Banna <tahrimabannadr23@gmail.com>
2024-10-06 22:57:29 +0600

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 bool prime(int n){
-    if(n<2)return true;
+    if(n<2)return false;
     if(n<=3)return true;
     if(n%2==0)return false;
 
-    for(int i=3;i*i<n;i+=2){
+    for(int i=3;i*i<=n;i+=2){
 
         if(n%i==0)return false;
     }
